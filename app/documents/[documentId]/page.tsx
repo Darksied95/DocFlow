072482aef32a729c8d04804bada3c4d81eced4fd
Aliyu Efe <aliyuefe9501@gmail.com>
2024-12-30 22:42:42 +0100

    add toolbar component and update dependencies

--- app/documents/[documentId]/page.tsx
+++ app/documents/[documentId]/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Editor } from './editor'
+import Toolbar from './toolbar'
 
 const DocumentIdPage = async ({ params }: { params: Promise<{ documentId: string }> }) => {
     const { documentId } = await params
     return (
         <div className="min-h-screen bg-[#FAFBFD]">
-
+            <Toolbar />
             <Editor />
         </div>
     )
